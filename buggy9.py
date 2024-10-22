91509cd06ce27941fd0b53345fe42af91b4843a8
MkGee <matthewrmcgeehan2@gmail.com>
2024-10-22 11:03:26 -0400

    Create buggy9.py

---
+++ buggy9.py
@@ -0,0 +1,17 @@
+#THERE ARE 5 BUGS
+
+define calculate_average(numbers)
+    if not numbers:
+        return "The list is empty, cannot calculate the average."
+    total = 0
+    for number in numbers:
+        total += number
+    average == total / len(numbers)
+    return average
+
+#list of numbers
+numbers = [10 + 20 + 30 + 40 + 50]
+
+#calculate and print the average
+print("The average is: , calculate_average(numbers))
+
